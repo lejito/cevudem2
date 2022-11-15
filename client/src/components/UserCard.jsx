95c6d41747f54cc7537d88a6602f64d9831dee73
alejocrrs <alejocrrs@gmail.com>
2022-11-14 23:57:07 -0500

    Update UserCard.jsx

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -53,7 +53,7 @@ function UserCard({ user }) {
                             {user.rol.charAt(0).toUpperCase() + user.rol.slice(1)}
                         </p>
                         <Link to={`./edit/${user.documento}`}>
-                            <button className="flex items-center justify-between px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-orange border border-transparent rounded-lg hover:bg-orange focus:outline-none focus:shadow-outline-purple">
+                            <button className="flex items-center justify-between px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-primary border border-transparent rounded-lg hover:bg-primary focus:outline-none focus:shadow-outline-purple">
                                 <svg className="w-4 h-4 mr-2 ml-1" fill="currentColor" aria-hidden="true" viewBox="0 0 20 20">
                                     <FontAwesomeIcon icon={faPencil}></FontAwesomeIcon>
                                 </svg>
@@ -79,10 +79,10 @@ function UserCard({ user }) {
                             {user.primer_nombre + " " + (user.segundo_nombre ? user.segundo_nombre : "") + " " + user.primer_apellido + " " + (user.segundo_apellido ? user.segundo_apellido : "")}
                         </p>
                         <p className="mb-2 text-sm font-medium italic text-gray-700">
-                            {user.rol.charAt(0).toUpperCase() + user.rol.slice(1)}
+                            {user.rol.charAt(0).toUpperCase() + user.rol.slice(1)} (Bloqueado)
                         </p>
                         <Link to={`./edit/${user.documento}`}>
-                            <button className="flex items-center justify-between px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-orange border border-transparent rounded-lg hover:bg-orange focus:outline-none focus:shadow-outline-purple">
+                            <button className="flex items-center justify-between px-3 py-1 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-secondary border border-transparent rounded-lg hover:bg-secondary focus:outline-none focus:shadow-outline-purple">
                                 <svg className="w-4 h-4 mr-2 -ml-1" fill="currentColor" aria-hidden="true" viewBox="0 0 20 20">
                                     <FontAwesomeIcon icon={faPencil}></FontAwesomeIcon>
                                 </svg>
